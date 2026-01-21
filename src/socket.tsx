ac89d06c829787052f29e5d234bc8e61d1b75acc
Hiren Shah <hirenshah026@gmail.com>
2026-01-21 12:44:32 +0530

    hash route changes

--- src/socket.tsx
+++ src/socket.tsx
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://listee-backend.onrender.com", {
   transports: ["websocket"],
-  autoConnect: false, // 🔴 VERY IMPORTANT
+  autoConnect: true,
 });
 
 export default socket;

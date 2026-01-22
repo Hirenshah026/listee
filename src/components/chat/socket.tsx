4acc81b77067094215d2ca2d543cb5bd2d06610b
Rajesh Kumar Singh Node Dev <rkumarcs911@gmail.com>
2026-01-22 14:21:46 +0530

    Update socket.tsx

--- src/components/chat/socket.tsx
+++ src/components/chat/socket.tsx
@@ -3,12 +3,14 @@ import { io } from "socket.io-client";
 // Space ko remove kar diya gaya hai aur connection options optimize kiye hain
 const API_URL = "https://listee-backend.onrender.com";
 const socket = io(API_URL, {
-  transports: ["websocket"],
+  transports: ["polling","websocket"],
+  withCrendentials:true,
   autoConnect: true,
   reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1000,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 2000,
 });
 
 export default socket;
+
 
